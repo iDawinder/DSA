77698942fbb4670d4b75d850cac3a82060ea3eaa
iDawinder <sdawinder653@gmail.com>
2026-02-02 17:04:08 +0530

    floyd triangle pattern added

---
+++ AdvancedPatterns/FLOYD_triangle/floydTriangle.java
@@ -0,0 +1,20 @@
+
+
+public class floydTriangle {
+    public static void FloydTriangle(int n){
+
+        int counter=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(counter + " ");
+                counter ++;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n=5;
+        FloydTriangle(n);
+    }
+}
