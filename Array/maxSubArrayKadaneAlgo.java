1ba85d6019fafc1a77339b7529ded24a90bec9ae
iDawinder <sdawinder653@gmail.com>
2026-02-11 23:34:11 +0530

    maximum sum of sub array with the KADANE'S algorithm

---
+++ Array/maxSubArrayKadaneAlgo.java
@@ -0,0 +1,24 @@
+
+public class maxSubArrayKadaneAlgo {
+
+    public static void kadanes(int numbers[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            cs = cs + numbers[i];
+            if (cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+
+        }
+        System.out.println("our maximum sum is " + ms);
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
+        kadanes(numbers);
+    }
+}
+
+//code not work for all the negative value in array
