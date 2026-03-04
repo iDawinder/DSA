9dfbbf928ebf84e617a7f1282ed5830d37d96a22
iDawinder <sdawinder653@gmail.com>
2026-03-04 22:18:46 +0530

    Counting sort added

---
+++ BasicSorting/CountingSort.java
@@ -0,0 +1,37 @@
+public class CountingSort {
+
+    public static void countingSort(int arr[]) {
+
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        int count[] = new int[max + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 4, 1, 3, 2, 4, 3, 7};
+
+        countingSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
