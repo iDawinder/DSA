6843381c41e29e74c38cb0650e46aac026b53cc2
iDawinder <sdawinder653@gmail.com>
2026-06-11 16:46:47 +0530

    minor changes made

--- Strings/calculateShortestpath.java
+++ Strings/calculateShortestpath.java
@@ -34,3 +34,31 @@ public class calculateShortestpath {
         System.out.println(Finalresult);
     }
 }
+
+/*
+Step	Direction	Valuw (x,y)
+Start	             (0,0)
+W	    x-1	        (-1,0)
+N	    y+1	        (-1,1)
+E	    x+1	        (0,1)
+E	    x+1	        (1,1)
+N	    y+1	        (1,2)
+E	    x+1	        (2,2)
+S	    y-1	        (2,1)
+E	    x+1	        (3,1)
+N	    y+1	        (3,2)
+N	    y+1	        (3,3)
+N	    y+1	        (3,4)
+
+at the end we get 3 4 and apply the formula of x2square + x1square whole square +
+ y2square + y1square whole square
+
+ N means up so y+1
+ S means down so y -1 
+
+ W is left so x-1 
+ E is right so x+1 
+
+ this is graphical representation on x and y axis 
+
+*/
